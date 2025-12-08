51f055c1e23e916ec81441b1278275e99c93de48
dawoodofficial04 <dawoodhussain423@gmail.com>
2025-12-08 12:14:35 +0500

    Header completed

--- src/Components/UserLayout/UserLayout.jsx
+++ src/Components/UserLayout/UserLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../Header'
 
 const UserLayout = () => {
   return (
     <div>
       {/* Header */}
+      <Header />
       {/* Main Content */}
       <main>
         <Outlet />
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export { default as UserLayout } from "./UserLayout"
+export { default as Header } from './Header'
+export { default as Topbar } from "./Topbar"
+export { default as Navbar } from "./Navbar"
