f2e6d0b970f733c3fc540a7981160403ae753208
Muhammad Arham <arhamofficial28@gmail.com>
2025-12-09 14:23:55 +0500

    components spelling changed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Topbar } from '../Topbar'
+import { Navbar } from '../Navbar'
+
+const Header = () => {
+  return (
+    <header className='border-b border-gray-200'>
+      {/* Topbar */}
+      <Topbar />
+      {/* Navber */}
+      <Navbar />
+      {/* Cart Drawer */}
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/UserLayout/UserLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../Header'
+import { Footer } from '../Footer'
+
+const UserLayout = () => {
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      {/* Main Content */}
+      <main>
+        <Outlet />
+      </main>
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
+}
+
+export default UserLayout
