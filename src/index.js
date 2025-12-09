8b42596924e9866144950d48d98c923a5ed09f60
dawoodofficial04 <dawoodhussain423@gmail.com>
2025-12-09 15:54:48 +0500

    Hero-Development merged into main

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as Header } from './Header'
 export { default as Topbar } from "./Topbar"
 export { default as Navbar } from "./Navbar"
 export { default as Home } from './Home'
+export { default as Hero } from './Hero'
